91cc1ece9bfe5437f0bddee84df1be703562acc9
Joe Cserko <j.cserko@gmail.com>
2016-09-14 11:22:11 -0400

    auto commit

--- git.js
+++ git.js
@@ -9,45 +9,45 @@ var testFn = (command) => {
     console.log(command);
 };
 
-async.series([
-    (callback) => {
-        let cmd = 'git add --all';
-        exec(cmd, function(error, stdout, stderr) {
-            if (error) {
-                callback(error);
-            } else {
-                callback(null, 'add ok');
-            }
-        });
-    },
-    (callback) => {
-        let cmd = 'git commit --all -m "auto commit"';
-        exec(cmd, function(error, stdout, stderr) {
-            if (error) {
-                callback(error);
-            } else {
-                callback(null, 'commit ok');
-            }
-        });
-    },
-    (callback) => {
-        let cmd = 'git push';
-        exec(cmd, function(error, stdout, stderr) {
-            if (error) {
-                callback(error);
-            } else {
-                callback(null, 'push ok');
-            }
-        });
-    }
-]);
+var startSync = () => {
+    async.series([
+        (callback) => {
+            let cmd = 'git add --all';
+            exec(cmd, function(error, stdout, stderr) {
+                if (error) {
+                    console.error('Error: ' + stdout);
+                    callback(error);
+                } else {
+                    console.log('Done: ' + stdout);
+                    callback(null, 'add ok');
+                }
+            });
+        },
+        (callback) => {
+            let cmd = 'git commit --all -m "auto commit"';
+            exec(cmd, function(error, stdout, stderr) {
+                if (error) {
+                    console.error('Error: ' + stdout);
+                    callback(error);
+                } else {
+                    console.log('Done: ' + stdout);
+                    callback(null, 'commit ok');
+                }
+            });
+        },
+        (callback) => {
+            let cmd = 'git push';
+            exec(cmd, function(error, stdout, stderr) {
+                if (error) {
+                    console.error('Error: ' + stdout);
+                    callback(error);
+                } else {
+                    console.log('Done: ' + stdout);
+                    callback(null, 'push ok');
+                }
+            });
+        }
+    ]);
+};
 
-// async.map(commands, testFn, function(err, results){
-//     console.log(results);
-// });
-
-// async.eachSeries(commands, function iteratee(item, callback) {
-//     async.setImmediate(function () {
-//         callback(null, item);
-//     });
-// });
+setInterval(startSync, (5*60));
