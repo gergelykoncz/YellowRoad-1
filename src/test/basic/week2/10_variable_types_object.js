4cba96019c415dc291d650444a706adb2919dce0
cherryApp <j.cserko@gmail.com>
2016-09-19 08:57:14 +0200

    Feat: upgrade object test.

--- src/test/basic/week2/10_variable_types_object.js
+++ src/test/basic/week2/10_variable_types_object.js
@@ -2,17 +2,19 @@ QUnit.test( "Változó Létezése", function( assert ) {
     assert.equal(
         false,
         typeof User === 'undefined',
-        "A User változó létezik és van értéke?"
+        "A User objektum létezik és van értéke?"
     );
 });
 
 // #
 
-QUnit.test( "Objektum name tulajdonság", function( assert ) {
-    assert.equal(
-        false,
-        typeof User.name === 'undefined',
-        "A User objektumnak van name tulajdonsága?"
+QUnit.test( "Objektum értékek vizsgálata", function( assert ) {
+    assert.ok(
+        User.name === 'Balázs' && 
+        User.age === 22 && 
+        User.address === 'Budapest' && 
+        User.isActive === true, 
+        "A User objektum tulajdonságai helyesek?"
     );
 });
 
@@ -22,21 +24,20 @@ QUnit.test( "Objektum age tulajdonság", function( assert ) {
     assert.equal(
         false,
         typeof User.age === 'undefined',
-        "A User objektumnak van age tulajdonsága?"
+        "A User objektum tulajdonságai megfelelnek az adott specifikációnak?"
     );
 });
 
 // #
 
-QUnit.test( "Objektum Tulajdonságok", function( assert ) {
-    var testUser = {
-        name: 'Joe',
-        age: 35
-    };
-
-    assert.deepEqual(
-        testUser,
-        User,
-        "Az objektum tulajdonságok értékei megfelelnek az adott specifikációnak?"
+QUnit.test( "Objektum kulcsok helyes használata", function( assert ) {
+    assert.ok(
+        /.*User\.name[ ]*\=[ ]*[\'\"]Balázs[\'\"].*/.test(currentEditorValue) && 
+        /.*User\.age[ ]*\=[ ]*22.*/.test(currentEditorValue),
+        "Felül definiálásnál szabályosan használta az objektum kulcsokat?"
     );
 });
+
+// #
+
+showModalMessage('<h4>A megoldásod helyes.</h4>', 'Gratulálok');
--- src/test/basic/week2/10_variable_types_object.md
+++ src/test/basic/week2/10_variable_types_object.md
@@ -1,9 +1,25 @@
 # Objektum létrehozása
+  
+> Ne feledd, az objektumnak tulajdonságára ponttal hivatkozunk, pld:  
+  obj.active = true;
+  
+- Definiálj egy objektumot:  
+  neve: User,  
+  értékei:  
+  - name: 'István',  
+  - job: 'teacher',  
+  - age: 40,  
+  - address: 'Budapest',  
+  - isActive: true  
+  
+- Definiáld felül a User objektumot:  
+  - a name tulajdonság legyen: 'Balázs',  
+  - az age tulajdonság legyen: 22  
 
-- Hozz létre egy objektumot "User" néven.  
-- Legyen az objektumnak két tulajdonsága:  
-  name, age.  
-- A tulajdonságok értékei a következőek legyenek:  
-  'Joe', 35.  
+- Definiálj egy új tulajdonságot a User objektumnak:  
+  - neve: youngFromCapital,  
+  - értéke: logikai kifejezés, ami akkor igaz ha az age kisebb mint 30 
+  és az address 'Budapest'   
 
-__Figyelj a helyes szintaktikára.__
+__Tipp 1: Figyelj a helyes szintaktikára.__  
+__Tipp 2: Lépésről lépésre dolgozz, ne fejben számolj.__  
